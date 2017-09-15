e1f25542b27ff64becfec2392bee223251c92e75
Rafal Nowosielski <rafal@nowosielski.email>
2017-09-15 21:36:00 +0200

    More clairty when the apiGateway property is empty.

--- src/index.js
+++ src/index.js
@@ -7,25 +7,26 @@ module.exports = function (req, res, next) {
     let segment = AWSXRay.getSegment();
     let event = req.apiGateway ? req.apiGateway.event : null;
 
-    if (event == null) {
+    if (event === null) {
       winston.info("Make sure to add the annot8 middleware after call to awsServerlessExpressMiddleware.eventContext()");
-    }
-    try {
-      let query = event.queryStringParameters ? `?${Object.keys(event.queryStringParameters).map(k => `${k}=${event.queryStringParameters[k]}`).join('&')}` : "";
-      segment.addAnnotation("Method", event.httpMethod);
-      segment.addAnnotation("Url", (event.headers.Host.endsWith(".cimpress.io")) ? `${event.headers.Host}${event.path}` : `${event.headers.Host}/${event.requestContext.stage}${event.path}${query}`);
-      segment.addAnnotation("ClientIp", event.requestContext.identity.sourceIp);
-      segment.addAnnotation("UserAgent", event.headers["User-Agent"]);
-      segment.addAnnotation("PrincipalId", event.requestContext.authorizer.principalId);
-      segment.addAnnotation("Authorization", event.headers.Authorization);
+    } else {
+      try {
+        let query = event.queryStringParameters ? `?${Object.keys(event.queryStringParameters).map(k => `${k}=${event.queryStringParameters[k]}`).join('&')}` : "";
+        segment.addAnnotation("Method", event.httpMethod);
+        segment.addAnnotation("Url", (event.headers.Host.endsWith(".cimpress.io")) ? `${event.headers.Host}${event.path}` : `${event.headers.Host}/${event.requestContext.stage}${event.path}${query}`);
+        segment.addAnnotation("ClientIp", event.requestContext.identity.sourceIp);
+        segment.addAnnotation("UserAgent", event.headers["User-Agent"]);
+        segment.addAnnotation("PrincipalId", event.requestContext.authorizer.principalId);
+        segment.addAnnotation("Authorization", event.headers.Authorization);
 
-      segment.addAnnotation("Stage", event.requestContext.stage);
+        segment.addAnnotation("Stage", event.requestContext.stage);
 
-      segment.addMetadata("query", event.queryStringParameters);
-      segment.addMetadata("headers", event.headers);
+        segment.addMetadata("query", event.queryStringParameters);
+        segment.addMetadata("headers", event.headers);
 
-    } catch (err) {
-      winston.error(err);
+      } catch (err) {
+        winston.error(err);
+      }
     }
   }
   if (next && typeof next === 'function') {
