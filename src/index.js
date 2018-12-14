a88f5ad8678423408be988fe5444da26eec0be85
Ivan Stanishev <istanishev@cimpress.com>
2018-12-14 11:28:22 +0100

    Add more checks

--- src/index.js
+++ src/index.js
@@ -1,36 +1,57 @@
 const winston = require("winston");
 const AWSXRay = require('aws-xray-sdk-core');
 
+function go(next) {
+    if (next && typeof next === 'function') {
+        next();
+    }
+}
+
 module.exports = function (req, res, next) {
+  if (process.env.xray !== "true") {
+    go(next);
+    return;
+  }
 
-  if (process.env.xray === "true") {
+  let event = req.apiGateway ? req.apiGateway.event : null;
+  if (event === null) {
+    winston.info("Make sure to add the annot8 middleware after call to awsServerlessExpressMiddleware.eventContext()");
+    go(next);
+    return;
+  }
+
+  try {
+    let query = event.queryStringParameters ? `?${Object.keys(event.queryStringParameters || {}).map(k => `${k}=${event.queryStringParameters[k]}`).join('&')}` : "";
+
+    let requestContext = event.requestContext || {};
+
     let segment = AWSXRay.getSegment();
-    let event = req.apiGateway ? req.apiGateway.event : null;
+    segment.addAnnotation("Method", event.httpMethod);
 
-    if (event === null) {
-      winston.info("Make sure to add the annot8 middleware after call to awsServerlessExpressMiddleware.eventContext()");
-    } else {
-      try {
-        let query = event.queryStringParameters ? `?${Object.keys(event.queryStringParameters).map(k => `${k}=${event.queryStringParameters[k]}`).join('&')}` : "";
-        segment.addAnnotation("Method", event.httpMethod);
-        segment.addAnnotation("Url", (event.headers.Host.endsWith(".cimpress.io")) ? `${event.headers.Host}${event.path}` : `${event.headers.Host}/${event.requestContext.stage}${event.path}${query}`);
-        segment.addAnnotation("ClientIp", event.requestContext.identity.sourceIp);
-        segment.addAnnotation("UserAgent", event.headers["User-Agent"]);
-        if (event && event.requestContext && event.requestContext.authorizer) {
-          segment.addAnnotation("PrincipalId", event.requestContext.authorizer.principalId);
-        }
-        segment.addAnnotation("Authorization", event.headers.Authorization);
-        segment.addAnnotation("Stage", event.requestContext.stage);
+    if (event.headers) {
+      segment.addMetadata("headers", event.headers);
 
-        segment.addMetadata("query", event.queryStringParameters);
-        segment.addMetadata("headers", event.headers);
+      let host = event.headers.Host || '';
+      segment.addAnnotation("Url", (host.endsWith(".amazonaws.com")) ? `${host}/${requestContext.stage}${event.path}${query}` : `${host}${event.path}`);
 
-      } catch (err) {
-        winston.error(err);
+      segment.addAnnotation("UserAgent", event.headers["User-Agent"]);
+
+      if (event.headers.Authorization) {
+          segment.addAnnotation("Authorization", event.headers.Authorization);
       }
     }
+
+    segment.addAnnotation("ClientIp", (requestContext.identity || {}).sourceIp);
+    if (requestContext.authorizer) {
+      segment.addAnnotation("PrincipalId", requestContext.authorizer.principalId);
+    }
+
+    segment.addAnnotation("Stage", requestContext.stage);
+    segment.addMetadata("query", event.queryStringParameters);
+
+  } catch (err) {
+    winston.error(err);
   }
-  if (next && typeof next === 'function') {
-    next();
-  }
+
+  go(next);
 };
